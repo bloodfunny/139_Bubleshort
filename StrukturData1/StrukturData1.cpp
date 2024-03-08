50c5c38e444e372bd39d91aeed8ae4f963442535
bloodfunny <adhyaakmalrafiansyah@email.com>
2024-03-08 11:11:46 +0700

    input banyaknya array

--- StrukturData1/StrukturData1.cpp
+++ StrukturData1/StrukturData1.cpp
@@ -2,4 +2,22 @@
 using namespace std;
 
 int a[20];
-int a;
+int n;
+
+void input() {
+	while (true) {
+		cout << "masukan banyaknya elemen pada array ! ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 20 elemen, \n";
+		}
+	}
+	cout << endl;
+	cout << "===================" << endl;
+	cout << "Masukan Elemen Array" << endl;
+	cout << "===================" << endl;
+
+
+}
